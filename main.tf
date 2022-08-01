4cdc20f27c1d8adab9fecce4889f81d77f32af37
Stefan Nader <stefan@nader.zone>
2022-08-01 15:19:44 +0200

    added test dummy

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# resource "aws_vpc" "test1" {
+#   cidr_block         = "10.0.0.0/16"
+#   enable_dns_support = true
+#   instance_tenancy   = "default"
+# }
